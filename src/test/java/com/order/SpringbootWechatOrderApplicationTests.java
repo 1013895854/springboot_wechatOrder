d492558363c73144cc9055f7d1fd3ac2deb094f7
1013895854 <Administrator@PC-20180925YQTA>
2019-07-30 22:05:18 +0800

    测试包路径 和 代码包路径一致

--- src/test/java/com/order/SpringbootWechatOrderApplicationTests.java
+++ src/test/java/com/order/SpringbootWechatOrderApplicationTests.java
@@ -1,16 +1,24 @@
-package com.show;
+package com.order;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootWechatOrderApplicationTests {
-
+	 private Logger logger = LoggerFactory.getLogger(SpringbootWechatOrderApplicationTests.class);
 	@Test
 	public void contextLoads() {
+		logger.error("error"+6);
+	}
+	
+	@Test
+	public void test() {
+		System.out.println();
 	}
 
 }
