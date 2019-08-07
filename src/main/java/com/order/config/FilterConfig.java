2f8ffced41659e8734ec980931dbbcc664a6d8b4
1013895854 <1013895854@qq.com>
2019-08-07 16:08:27 +0800

    filterConfig

--- src/main/java/com/order/config/FilterConfig.java
+++ src/main/java/com/order/config/FilterConfig.java
@@ -3,7 +3,6 @@ package com.order.config;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.filter.DelegatingFilterProxy;
 
 import com.order.until.xss.XssFilter;
 
